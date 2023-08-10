68c6b0be68c0d5fbcc669d93b3490984352e1e06
Catalin B <catalin.bac@gmail.com>
2023-08-10 12:01:03 +0300

    nav mobile working

--- dist/js/main.js
+++ dist/js/main.js
@@ -58,10 +58,17 @@ let layoutHandler;
   const getHamburger = document.querySelector('.hamburger');
   const getHamburgerSpan = document.querySelectorAll('.hamburger > span');
   const getEclipseMobileMenu = document.querySelector('.eclipse-menu-mobile');
+  const getBody = document.querySelector('body');
+
+
+
+
 
   const modifySpan = () => {
     getHamburger.addEventListener('click', () => {
       getEclipseMobileMenu.classList.toggle('show-mobile-menu');
+      getBody.classList.toggle("over-body")
+
       getHamburgerSpan.forEach(element => {
         element.classList.toggle('show');
       });
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,10 +7,17 @@
   const getHamburger = document.querySelector('.hamburger');
   const getHamburgerSpan = document.querySelectorAll('.hamburger > span');
   const getEclipseMobileMenu = document.querySelector('.eclipse-menu-mobile');
+  const getBody = document.querySelector('body');
+
+
+
+
 
   const modifySpan = () => {
     getHamburger.addEventListener('click', () => {
       getEclipseMobileMenu.classList.toggle('show-mobile-menu');
+      getBody.classList.toggle("over-body")
+
       getHamburgerSpan.forEach(element => {
         element.classList.toggle('show');
       });
