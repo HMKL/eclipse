297da8b1df73d32e2cfcd1e688eddc825a210f7c
Catalin B <catalin.bac@gmail.com>
2023-08-13 11:29:09 +0300

    some fixed on main header menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -59,6 +59,79 @@ let layoutHandler;
   const getHamburgerSpan = document.querySelectorAll('.hamburger > span');
   const getEclipseMobileMenu = document.querySelector('.eclipse-menu-mobile');
   const getBody = document.querySelector('body');
+  const getHoverMenu = document.querySelector('.hover-menu');
+  const getEclipseMenuHeader = document.querySelector('.eclipse-menu-header');
+  const getInfoH = document.querySelectorAll('.info > ul > li > span');
+  const getLargeInfo = document.querySelectorAll('.large-info > div');
+
+
+
+
+
+  function showMenuHeader() {
+
+    getInfoH[0].addEventListener('mouseenter', () => {
+      getLargeInfo[0].classList.add('show-div');
+      getLargeInfo[1].classList.remove('show-div');
+    });
+
+
+
+
+    getInfoH[1].addEventListener('mouseenter', () => {
+      getLargeInfo[1].classList.add('show-div');
+      getLargeInfo[0].classList.remove('show-div');
+    });
+
+
+
+
+
+
+
+    // getInfoH.forEach(element => {
+    //   element.addEventListener('mouseleave', () => {
+    //     getLargeInfo.forEach(element => {
+    //       element.classList.remove('show-div');
+    //     });
+    //   })
+    // });
+
+  }
+  showMenuHeader();
+
+
+
+
+
+
+
+
+
+
+
+  //Add class to main menu
+  const addClassToMenuHeader = () => {
+    getHoverMenu.addEventListener('mouseenter', () => {
+      getEclipseMenuHeader.classList.add('show-menu');
+    });
+  }
+
+  addClassToMenuHeader();
+
+
+
+
+  const removeClassToMenuHeader = () => {
+    getEclipseMenuHeader.addEventListener('mouseleave', () => {
+      getEclipseMenuHeader.classList.remove('show-menu');
+    });
+  }
+
+  removeClassToMenuHeader();
+
+  //End class to main menu
+
 
 
 
