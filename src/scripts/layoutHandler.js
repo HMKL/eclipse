88b60a3809e23197a0d78731983c8ed6bb5173a2
Catalin B <catalin.bac@gmail.com>
2023-08-17 11:46:57 +0300

    carousel added

--- src/scripts/layoutHandler.js
+++ src/scripts/layoutHandler.js
@@ -33,6 +33,33 @@ let layoutHandler;
 
 
 
+        var swiper = new Swiper(".mySwiper", {
+            slidesPerView: 1,
+
+            // loop: true,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+            breakpoints: {
+                320: {
+                    slidesPerView: 1.2,
+                    spaceBetween: 20
+                },
+                480: {
+                    slidesPerView: 3,
+                },
+                1024: {
+                    slidesPerView: 4,
+                    spaceBetween: 30
+                }
+            }
+        });
+
 
 
 
